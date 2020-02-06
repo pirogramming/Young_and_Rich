04b921ca21446371a81bf7ba7a2b995963ebdec4
teclados <wjdtmdwo07@daum.net>
2020-02-06 11:55:04 +0900

    '진행바'

--- comp/urls.py
+++ comp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import progressbar
 
 app_name = 'comp'
 
 urlpatterns = [
-
+    path('comp/progressbar/<int:pk>/', progressbar, name='progressbar'),
 ]
--- comp/views.py
+++ comp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from comp.models import Comp
+
+from datetime import datetime
+
+from django.utils.dateformat import DateFormat
+
+
+def progressbar(request, pk):
+    comp = get_object_or_404(Comp, pk=pk)
+
+    today = int(DateFormat(datetime.now()).format('Ymd'))
+
+    created_date = int(DateFormat(comp.created_at).format('Ymd'))
+    dead_date = int(DateFormat(comp.deadline.date()).format('Ymd'))
+    total = dead_date - created_date
+
+    context = {
+        'comp': comp
+    }
+    return render(request, 'comp/progressbar.html', context)
