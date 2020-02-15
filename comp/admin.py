d50d2ba176b0e87e99c5d8dd479a504e50158f32
Soohyun-CHOI <csh0sns@naver.com>
2020-02-15 14:21:48 +0900

    code까지

--- comp/admin.py
+++ comp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from comp.models import Comp, ComPost, ComComment
+from comp.models import Comp, ComPost, ComComment, CodePost, CodeComment
 
 admin.site.register(Comp)
+admin.site.register(CodePost)
+admin.site.register(CodeComment)
 admin.site.register(ComPost)
 admin.site.register(ComComment)
