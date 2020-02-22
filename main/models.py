7be671a7341de341383acc8f88fdc4ac62b47550
teclados <wjdtmdwo07@daum.net>
2020-02-22 13:46:07 +0900

    add test

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 
 class Notice(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     context = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
