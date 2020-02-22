c81ddf9f778d5642cf92ebd643437bb801f27c88
ByungHeonLEE <topkids5326@gmail.com>
2020-02-22 12:34:32 +0900

    찐찐막

--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main/main.html')
+
+
+def explanation_page(request):
+    return render(request, 'main/explanation_page.html')
+
+
+def explanation_competition(request):
+    return render(request, 'main/explanation_competition.html')
+
+
+def explanation_faq(request):
+    return render(request, 'main/explanation_faq.html')
