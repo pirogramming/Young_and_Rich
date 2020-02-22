1508fa4a01bb8102b36e977ad061c748f0ebe61a
ByungHeonLEE <topkids5326@gmail.com>
2020-02-22 13:29:17 +0900

    aaaa

--- main/views.py
+++ main/views.py
@@ -8,17 +8,17 @@ def main(request):
 
 
 def notice_list(request):
-    notice_list= Notice.objects.all()
-    ctx={
-        'notice_list':notice_list,
+    notice_list = Notice.objects.all()
+    ctx = {
+        'notice_list': notice_list,
     }
-    return render(request,'main/notice_list.html',ctx)
+    return render(request, 'main/notice_list.html', ctx)
 
 
 def notice_detail(request, pk):
     notice = Notice.objects.get(pk=pk)
-    notice.view_num+=1
+    notice.view_num += 1
     notice.save()
-    ctx={'notice':notice,
-         }
-    return render(request, 'main/notice_detail.html',ctx)
+    ctx = {'notice': notice,
+           }
+    return render(request, 'main/notice_detail.html', ctx)
